6f1e3236f35787444ceb51a43991a93db4b0f729
vue3course <vue3course.fa@gmail.com>
2021-01-29 15:39:18 +0330

    L4-Finish

---
+++ src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="event-card">
+    <span>راس ساعت 12:00 در تاریخ 1400/01/13</span>
+    <h4>روز درختکاری</h4>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card span {
+ font-size: 12px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <EventCard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    EventCard
   }
 };
 </script>
+
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
