e231d7f69887b13db649a280be2d269e2c222066
vue3course <vue3course.fa@gmail.com>
2021-02-19 13:22:05 +0330

    finish lesson7

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import BaseIcon from '@/components/BaseIcon.vue';
 
-createApp(App)
+const app = createApp(App)
+app.component('BaseIcon', BaseIcon)
   .use(store)
   .use(router)
-  .mount("#app");
+  .mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>این صفحه درباره ما است</h1>
+    <BaseIcon />
   </div>
 </template>
